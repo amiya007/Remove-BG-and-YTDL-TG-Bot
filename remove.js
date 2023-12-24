a23c8bda007d111e638c3b2674197d3c14d85d7e
amiya007 <amiya.boxi76625@gmail.com>
2023-12-24 16:53:49 +0530

    initial commit.

---
+++ remove.js
@@ -0,0 +1,11 @@
+// const  imglyRemoveBackground  = require("@imgly/background-removal-node");
+
+// async function removeBG(image_src) {
+//   imglyRemoveBackground(image_src).then((blob) => {
+//     // The result is a blob encoded as PNG. It can be converted to an URL to be used as HTMLImage.src
+//     const url = URL.createObjectURL(blob);
+//     console.log(url);
+//     return url;
+//   });
+// }
+// module.exports= removeBG;
