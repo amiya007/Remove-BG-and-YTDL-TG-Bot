249414a46fbffcef70b512ffc7b231ba2e05348d
amiya007 <amiya.boxi76625@gmail.com>
2023-12-24 21:26:40 +0530

    error handling.

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 console.log("Hello");
-const fs = require("fs");
 const dotenv = require("dotenv");
 const { Telegraf } = require("telegraf");
-const { message } = require("telegraf/filters");
-const { removeBackground } = require("@imgly/background-removal-node");
+const { replyPhoto } = require("./src/replyPhoto");
 // const removeBg =require("./remove.js");
 
 // dotenv.config();
@@ -13,78 +11,39 @@ const { removeBackground } = require("@imgly/background-removal-node");
 
 dotenv.configDotenv();
 console.log(process.env.TELEGRAM_API_KEY);
-let imgLink;
-
 const bot = new Telegraf(process.env.TELEGRAM_API_KEY);
+
 bot.start((ctx) => ctx.reply("Welcome"));
-bot.command("oldschool", (ctx) => ctx.reply("Hello"));
-// bot.on("message", (ctx) => {
-//   const files = ctx.update.message.photo;
-//   console.log(files);
-//   //pop() to get the last element
-//   let fileId = files.pop().file_id;
-//   console.log(fileId);
-// });
-bot.on(message("photo"), async (ctx) => {
-  ctx.telegram.sendChatAction(ctx.chat.id, "upload_photo");
-  let imageId = ctx.message.photo.pop().file_id;
 
-  await ctx.telegram.getFileLink(imageId).then((link) => {
-    //Download Image
-    // https.get(link, (response) =>
-    //     response.pipe(fs.createWriteStream(`${imageId}.jpeg`))
-    // );
-
-    console.log(link.href);
-    imgLink = link.href;
-    removeBackground(imgLink).then((blob) => {
-      // const url = URL.createObjectURL(blob.arrayBuffer);
-      blob.arrayBuffer().then((arrBuffer) => {
-        console.log(arrBuffer);
-        let data = arrBuffer; // you image stored on arrayBuffer variable;
-        data = Buffer.from(data);
-        fs.writeFile(`BG-removed.png`, data, async (err) => {
-          // Assets is a folder present in your root directory
-          if (err) {
-            console.log(err);
-          } else {
-            await ctx.telegram.sendDocument(
-              ctx.chat.id,
-
-              //for assets
-              { source: "BG-removed.png" },
-              //for URL
-              // { url: imgLink },
-              {
-                reply_to_message_id: ctx.message.message_id,
-                caption: "Here is your photo.",
-              }
-            );
-            ctx.sendMessage("Thank You for Using me💕");
-            console.log("File created successfully!");
-          }
-        });
-      });
-
-      // fs.createWriteStream(imageName).write(imageBuffer);
-      // console.log(url);
-    });
-  });
-
-  // await removeBg(imgLink);
-
-  // ctx.telegram.sendPhoto(
-  //   ctx.chat.id,
-
-  //   //for assets
-  //   //   { source: "assets/dog.jpg" },
-
-  //   { url: imgLink },
-  //   {
-  //     reply_to_message_id: ctx.message.message_id,
-  //     caption: "Here is your photo.",
-  //   }
-  // );
+// bot.command("oldschool", (ctx) => ctx.reply("Hello"));
+bot.use((ctx, next) => {
+  const message = ctx.message;
+  if (message.photo) {
+    console.log("It is a Photo");
+    replyPhoto(ctx);
+  } else if (message) {
+    const emojiRegex =
+      /[\u{1F300}-\u{1F5FF}\u{1F600}-\u{1F64F}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{1FA70}-\u{1FAFF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}]/gu;
+    const emojis = message.text.match(emojiRegex);
+    const youtubeRegex =
+      /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11}[^\s&]*)/;
+    const ytlink = message.text.match(youtubeRegex);
+    if (emojis) {
+      console.log(`emojis  ${emojis.join(" ")}`);
+      ctx.reply("👍");
+    } else if (message.text && !emojis) {
+      if (ytlink) {
+        console.log(`yt link msg ${ytlink[0]}`);
+      } else {
+        console.log(`message only ${message.text}`);
+      }
+    } else {
+      console.log("it is mixed");
+    }
+  } else if (message.sticker) {
+    console.log("it is sticker");
+  }
+  next();
 });
 
 bot.launch();
---
+++ src/replyPhoto.js
@@ -0,0 +1,52 @@
+const fs = require("fs");
+const { removeBackground } = require("@imgly/background-removal-node");
+
+let imgLink;
+async function replyPhoto(ctx) {
+  // bot.on(message("photo"), async (ctx) => {
+  ctx.telegram.sendChatAction(ctx.chat.id, "upload_photo");
+  let imageId = ctx.message.photo.pop().file_id;
+
+  await ctx.telegram.getFileLink(imageId).then((link) => {
+    //Download Image
+    // https.get(link, (response) =>
+    //     response.pipe(fs.createWriteStream(`${imageId}.jpeg`))
+    // );
+    console.log(link.href);
+    imgLink = link.href;
+    try {
+      removeBackground(imgLink).then((blob) => {
+        blob.arrayBuffer().then((arrBuffer) => {
+          console.log(arrBuffer);
+          data = Buffer.from(arrBuffer);
+          fs.writeFile(`BG-removed.png`, data, async (err) => {
+            if (err) {
+              console.log(err);
+              ctx.reply("Something went wrong.☹️\nPlease Try again later.");
+            } else {
+              try {
+                await ctx.telegram.sendDocument(
+                  ctx.chat.id,
+                  { source: "BG-removed.png" },
+                  {
+                    reply_to_message_id: ctx.message.message_id,
+                    caption: "Here is your photo.",
+                  }
+                );
+                ctx.sendMessage("Thank You for Using me💕");
+                console.log("File created successfully!");
+              } catch (error) {
+                ctx.reply("Something went wrong.☹️ \nPlease Try again later.");
+              }
+            }
+          });
+        });
+      });
+    } catch (error) {
+      console.log(`err :: ${error}`);
+      ctx.reply("Something went wrong.☹️ \nPlease Try again later.");
+    }
+  });
+  // });
+}
+exports.replyPhoto = replyPhoto;
